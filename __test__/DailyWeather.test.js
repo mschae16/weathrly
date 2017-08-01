89981e240da9942206fa1de1fa2a66110179f8e3
Margo Schaedel <margoschaedel@gmail.com>
2017-08-01 17:11:08 -0600

    Add stateless component testing

--- __test__/DailyWeather.test.js
+++ __test__/DailyWeather.test.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import DailyWeather from '../lib/components/DailyWeather/DailyWeather';
+import DailyCard from '../lib/components/DailyCard/DailyCard'
+import mockData from '../__test-helpers__/mockData.js';
 
 describe('DailyWeather', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<DailyWeather />);
+    wrapper = mount(<DailyWeather
+      tenDay={mockData.tenDay} />);
   });
 
   it('should exist', () => {
     expect(wrapper).toBeDefined();
+  });
+
+  it('should have a container div with className of Daily-card-container', () => {
+    const dailyWeather = wrapper.find('.Daily-card-container');
+    expect(dailyWeather).toHaveLength(1);
+  })
+
+  it('should render ten daily weather cards', () => {
+    const tenDay = wrapper.find(DailyCard);
+    expect(tenDay).toHaveLength(10);
   });
 });
--- __test__/HourlyWeather.test.js
+++ __test__/HourlyWeather.test.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import HourlyWeather from '../lib/components/HourlyWeather/HourlyWeather';
+import HourlyCard from '../lib/components/HourlyCard/HourlyCard';
+import mockData from '../__test-helpers__/mockData.js';
 
 describe('HourlyWeather', () => {
   let wrapper;
 
-  const data
-
   beforeEach(() => {
-    wrapper = shallow(<HourlyWeather />);
+    wrapper = mount(<HourlyWeather
+      hourlyData={mockData.hourlyData} />);
   });
 
   it('should exist', () => {
     expect(wrapper).toBeDefined();
+  });
+
+  it('should have a container div with className of Hourly-card-container', () => {
+    const hourlyWeather = wrapper.find('.Hourly-card-container');
+    expect(hourlyWeather).toHaveLength(1);
+  })
+
+  it('should render seven hourly weather cards', () => {
+    const hourlyWeather = wrapper.find(HourlyCard);
+    expect(hourlyWeather).toHaveLength(7);
   });
 });
